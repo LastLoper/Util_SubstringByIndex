0bfaca242c0845c88034b61e7c1ae66a8219a523
LastLoper <ys.mobility@gmail.com>
2022-10-18 22:30:47 +0900

    update

--- AutoHTTP.playground/Contents.swift
+++ AutoHTTP.playground/Contents.swift
@@ -1,12 +1,36 @@
 import UIKit
 
-//'http://' 자동 삽입
+//'http://' or 'https://' 자동 삽입
+//함수
 func addHTMLAuto(_ url:String) -> String {
-    var strUrl = url
-    let flag = strUrl.hasPrefix("http://")
+    var strUrl = ""
+    var flag = false
+    
+    if strUrl.hasPrefix("http://") || strUrl.hasPrefix("https://") {
+        flag = true
+    }
+    
     if (!flag) {
-        strUrl = "http://" + strUrl
+        strUrl = "http://" + url
     }
     
     return strUrl
+}
+
+//extesion
+extension String {
+    //'http://' 자동 삽입
+    func addHTMLAuto() -> String {
+        var strUrl = ""
+        var flag = false
+        if self.hasPrefix("http://") || self.hasPrefix("https://") {
+            flag = true
+        }
+        
+        if (!flag) {
+            strUrl = "http://" + self
+        }
+        
+        return strUrl
+    }
 }
